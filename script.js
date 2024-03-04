826195a4a749ce0de3f0148a4248fa4d5551c0f2
inclined4X <jezemiahsam48@gmail.com>
2024-03-04 22:15:24 +0000

    passing arguments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// DEFAULT PARAMETERS
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +28,15 @@ createBooking('LH123', 5);
 
 // We cant do this
 createBooking('LH123', undefined, 1000);
+*/
+
+// HOW PASSING ARHUMENTS WORKS VALUE VS REFERENCE
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  password: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {};
+
+checkIn();
