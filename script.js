45896c6f18eaaeade40d77a7bd1d8febbdc9320b
inclined4X <jezemiahsam48@gmail.com>
2024-03-04 13:38:41 +0000

    Changed the array name to bookings and tried doing doing default parameters with ES6

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const booking = [];
+const bookings = [];
 
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 1991;
   const booking = {
     flightNum,
     numPassengers,
     price,
   };
   console.log(booking);
-  booking.push(booking);
+  bookings.push(booking);
 };
 
 createBooking('LH123');
