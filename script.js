55288f66a65a78ca6d681830e2c2a6f6cfdb8d19
inclined4X <jezemiahsam48@gmail.com>
2024-03-05 11:02:32 +0000

    making bookings

--- script.js
+++ script.js
@@ -128,10 +128,23 @@ greetArr2('Hi')('Jezemiah');
 const Lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
-  booking: [],
+  bookings: [],
   book(flightNum, name) {
     console.log(
       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
     );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
   },
 };
+
+Lufthansa.book(239, 'Jonas Schmeddtmann');
+Lufthansa.book(635, 'Jezemiah');
+console.log(Lufthansa);
+
+const eurowings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  booking: [],
+};
+
+const book = Lufthansa.book;
