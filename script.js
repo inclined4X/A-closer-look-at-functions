da5712bc6649b26ecce1695b9398fc01426175b0
inclined4X <jezemiahsam48@gmail.com>
2024-03-05 00:50:53 +0000

    abstraction

--- script.js
+++ script.js
@@ -87,8 +87,11 @@ const transformer = function (str, fn) {
 transformer('JavaScript is the best!', upperFirstWord);
 transformer('JavaScript is the best!', oneWord);
 
+// JS uses callbacks all the time
 const high5 = function () {
   console.log('👌');
 };
 
 document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
