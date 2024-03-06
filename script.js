c30de35b5ed48ed7e4b3786296c782a0c189d7c6
inclined4X <jezemiahsam48@gmail.com>
2024-03-06 00:03:08 +0000

    solve the tax problem with returning function

--- script.js
+++ script.js
@@ -209,3 +209,16 @@ const addTax = (rate, value) => value + value * rate;
 console.log(addTax(0.1, 200));
 
 const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+console.log(addVAT(23));
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(23));
+console.log(addVAT2(100));
